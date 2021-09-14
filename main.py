08cf4eafb666bb8ac720702fb6a9977e49b91342
pedro-design <84478860+pedro-design@users.noreply.github.com>
2021-09-13 19:26:22 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import math 
 import random
 import nn
-from timeit import timeit
-from nn import neural_network, error,normalize
+#from timeit import timeit
+from nn import neural_network, error,normalize,random_float
 #import machine
 #machine.freq(160000000)
 #dummy wheater  dataset
-dummy = [100,110]
+dummy = [100,110,102]
 DdatasetX = []
-DdatasetY = [[100,110]]
-nsamples = 300
+DdatasetY = [[100,86,110]]
+nsamples = 90
 for i in range(nsamples):
     if random.getrandbits(2) == 1:
-        dummy=[dummy[0]+random.getrandbits(4)-random.getrandbits(4),dummy[1]+random.getrandbits(4)-random.getrandbits(4)]
+        dummy=[dummy[0]+random.getrandbits(4)-random.getrandbits(4),dummy[1]+random.getrandbits(4)-random.getrandbits(4),dummy[2]+random.getrandbits(4)-random.getrandbits(4)]
     else:
-        dummy=[dummy[0]-random.getrandbits(4)+random.getrandbits(4),dummy[1]-random.getrandbits(4)+random.getrandbits(4)]
+        dummy=[dummy[0]-random.getrandbits(4)+random.getrandbits(4),dummy[1]-random.getrandbits(4)+random.getrandbits(4),dummy[2]-random.getrandbits(4)+random.getrandbits(4)]
     DdatasetX.append(dummy)
 for i in range(1,len(DdatasetX)):
     DdatasetY.append(DdatasetX[i])
 DdatasetY = normalize(DdatasetY)#normalize to 0-1 for prevent gradients exploding
-DdatasetX = normalize(DdatasetY)
+DdatasetX = normalize(DdatasetX)
 print('test dataset ',end='')# check if the dataset x and y have the same len
+print(len(DdatasetX[3]),'H',len(DdatasetY[3]))
 if len(DdatasetX) == len(DdatasetY):
     print("ok")
     test1 = True 
@@ -28,8 +29,8 @@ else :
     test1 = False
     print('bad dataset')
 
-        
-nn = neural_network(4,[2,2,2,2],0.00015,0.01)#define neural net smaller leraning rate more iter will take to end
+#create the nn :neural_network(number of layers,[layers ex: 3,3,3],lr,momemtum)
+nn = neural_network(4,[6,6,6,3],0.15,0.01)#define neural net smaller leraning rate more iter will take to end
 print(nn.foward(DdatasetX[1]))#get a prediction from a random init net
 #for plotting:
 
@@ -37,35 +38,46 @@ print(nn.foward(DdatasetX[1]))#get a prediction from a random init net
 #tmperrs = []#tmp error list
 ###################
 end = False
-iterations = 100
+#iterations = 100
 c = 0
-errls13  =nn.train(DdatasetX,DdatasetY,10,stop=0.01)#usaje neural-net.train(X,Y,proggress print,stop value for loss)
+errls13  =nn.train(DdatasetX,DdatasetY,50,stop=0.01,lr_steps = 500)#usaje neural-net.train(X,Y,proggress print,stop value for loss,lr_reduce steps)
 
 #nn.save("iris")
 
 #-------evaluate-------
 future = []
-
-
+future1 = []
+nn.save("tutorial1",save_train_val = True)
+nn_loaded = neural_network(2,[2,2],0.015,0.0001)#build a holder nn
+nn_loaded.load("tutorial1")#load rebuilds the nn
 for i in range(10):#iterating over the dataset
     print("predicted: ",nn.foward(DdatasetX[i]),"real: ",DdatasetY[i])
 print("future test:")
 values=nn.foward(DdatasetX[29])
-value = [values[0],values[1]]
+
+value = [values[0]-random_float()/100,values[1]-random_float()/100,values[2]-random_float()/100]
 for i in range(len(DdatasetX)):
     values=nn.foward(value)
+    values1 = nn_loaded.foward(value)
     print("predicted step {}: ".format(i),value)
-    value = [values[0]+random.getrandbits(1)-random.getrandbits(1),values[1]+random.getrandbits(1)-random.getrandbits(1)]#add noise
+    value1 = [values[0],values[1],values[2]]
+    value = [values[0]+random_float(),values[1]+random_float(),values[2]+random_float()]#add noise
     future.append(value)
+    future1.append(value1)
+#print(nn.weights)
+#print(nn.bias)
+#print(nn.derivatesW)
+#print(nn.momentum)
 #---------------plot data-------------------
 #optional section if not using in micropython
-import matplotlib.pyplot as plt
-plt.plot(errls13)
-plt.show()
-plt.plot(DdatasetY)
-plt.show()
-plt.plot(future)
-plt.show()
+#import matplotlib.pyplot as plt
+#plt.plot(errls13)
+#plt.show()
+#plt.plot(DdatasetY)
+#plt.show()
+#plt.plot(future)
+#plt.plot(future1)
+#plt.show()
 #plt.plot(prrls)
 #plt.show()
 
--- nn.py
+++ nn.py
@@ -1,6 +1,7 @@
 import math 
 import random
 #import pickle
+import json
 #-------helper functions -----------
 def normalize(V):
     res = []
@@ -13,6 +14,8 @@ def normalize(V):
         
         res.append([(j - min_)/(max_ - min_) for j in i])
     return res
+def random_float():
+    return (random.getrandbits(8)/255)
     
 def initW(nb):# init random weight
     W = []
@@ -72,27 +75,68 @@ class neural_network():
         self.weights[-1] = [initW(1) for q in range(nodes[-1])]
        # self.momentum = 0
         self.d = 0
-    def train(self,X,Y,iterations,autorun = False,stop=1):
+    def save(self,name,save_train_val = False):
+        self.outt = {}
+        if save_train_val == True:
+            self.outt["db"]  = self.derivatesW
+            self.outt["m"]  = self.momentum
+            self.outt["lr"] = self.lr
+        self.outt["w"]  = self.weights
+        self.outt["b"] = self.bias
+        self.outt["la"] = self.layers
+        self.outt["nodes"] = self.nl
+        self.outt["p"] = self.predictions
+        
+        with open("./"+name+".json","w") as Save:
+            json.dump(self.outt, Save)
+        del self.outt
+        print("saved nn {}".format(name))
+    def load(self,name):
+        with open("./"+name+".json","r") as SaveN:
+            self.data = json.load(SaveN)
+            print("loaded nn {}".format(name))
+            self.layers = self.data["la"]
+            self.nl = self.data["nodes"]
+           # self.__init__
+            try :
+                self.derivatesW = self.data["db"]
+                self.momentum = self.data["m"]
+                self.lr = self.data["lr"] 
+            except: pass
+            
+            
+            self.bias = self.data["b"]
+            self.weights = self.data["w"]
+            self.predictions = self.data["p"]
+            
+            del self.data
+    def train(self,X,Y,iterations,autorun = False,stop=1,lr_steps = 3 ):
         self.c = 0
         self.end = 0
         self.counter = 0
         self.counter1 = 0
         self.errls13 = []#error list for plotting
         self.ploss= 1
+        self.lr_steps = 0
         while(self.end == 0):
             self.tmperrs1 = []#tmp error list
             for w in range(len(X)):#pass over the dataset
-              self.foward(Y[w])#get predictions and update internal state
+             # print("jdnsnsn",X[w])
+              self.foward(X[w])#get predictions and update internal state
               try:
                   self.tmperrs1.append(sum(error(Y[w],self.foward(X[w]))))#get error of the nn and add to its list
               except:
+                 # print(X[w],w,'kjgjxjv')
                   self.tmperrs1.append(error(Y[w],self.foward(X[w])))
-              self.back(Y[w])#back propagation 
+        #      print(Y[w],w)
+            if (sum(self.tmperrs1) > 100 and self.counter >10 )or sum(self.tmperrs1) < 0.0001  or (len(self.errls13) > 3 and sum(self.tmperrs1) > sum(self.errls13[-2:]) ):#protection anti-nan
+                    print('gradients Exploding, ending')
+                    self.end = 1
+            else :
+                self.back(Y[w])#back propagation  
             if self.c == int(iterations):#simple progress bar
                     print("iter: {} loss: {} ".format(self.counter1,sum(self.tmperrs1)))
                     self.c = 0
-            
-            
             self.c = self.c+1
             if sum(self.tmperrs1) < stop:
                 self.end = 1
@@ -100,21 +144,27 @@ class neural_network():
             self.counter = self.counter +1
             self.counter1 = self.counter1 +1
             self.errls13.append(sum(self.tmperrs1))#get the sum of temp error list and add it to the error list
-            if self.ploss == sum(self.tmperrs1):
-                print("model not learning, end training")
+            if self.ploss == sum(self.tmperrs1) or self.ploss < sum(self.tmperrs1) :
+                print("model not learning, decreasing learning rate")
+                self.lr  = self.lr - (sum(self.tmperrs1)*0.00001)
+                self.m = self.m * 0.1
+                print("new lr ",self.lr)
+                self.lr_steps = self.lr_steps +1
+            if self.lr < 0 :
+                print("lr is ded, end training, lr value ",self.lr)
+                self.lr = -1 * self.lr
+                self.end = 1
+            if self.lr_steps ==lr_steps :
+                print("training done")
                 self.end = 1
             if self.counter > 2 :
+              #  self.end = 1
                 self.counter = 0
                 self.ploss = sum(self.tmperrs1)
-            if (sum(self.tmperrs1) > 100 and self.counter >10 )or sum(self.tmperrs1) < 0.0001  :#protection anti-nan
-                    raise Exception('gradients Exploding, try reducing the layers or changing lr and momentum')
-                    break
-            
-        
         print("#")
         print("last error: ",sum(self.tmperrs1))
         return self.errls13
-        textfile.close()
+      #  textfile.close()
     def foward(self,x):
         self.InputV = x
         if len(self.InputV) > len(self.weights[0]) :
@@ -125,22 +175,32 @@ class neural_network():
                 print("###### WARNING ######")
                 print("using minor number of neurons than the data feed, can cause undercoverage problems or slow learning")
                 print("")
-        elif len(self.InputV) < len(self.weights[0]) :
-              raise Exception('use the same number of input neurons as the data feed')
+        elif len(self.InputV)*2 == len(self.weights[0]) :
+            self.tc = 0 
+            for i in range(0,len(self.InputV),2):
+         #       print(i)
+                self.predictions[0][i]=dot([self.InputV[self.tc],self.InputV[self.tc+1]],self.weights[0][i])+self.bias[0][i]
+                self.tc = self.tc+1
+             # print(len(self.InputV))
+              #raise Exception('use the same number of input neurons as the data feed')
         else:
-            for i in range(len(self.InputV)):
+           for i in range(len(self.InputV)):
                 self.predictions[0][i]=dot([self.InputV[i]],self.weights[0][i])+self.bias[0][i]
-        for q in range(len(self.weights)):#iterate over layers
+        for q in range(1,len(self.weights)):#iterate over layers
             for x in range(len(self.weights[q])):
-                    self.predictions[q][x]=dot([self.InputV[e] for e in range(len(self.weights[q]))],self.weights[q][x])+self.bias[q][x]
+                    self.predictions[q][x]=dot([self.predictions[q-1][e] for e in range(len(self.weights[q]))],self.weights[q][x])+self.bias[q][x]
               
+        if len(self.predictions[-1]) == 1 :
+            return self.predictions[-1][0]#get outs
+        else :
+            return self.predictions[-1]
         if len(self.predictions[-1]) == 1 :
             return self.predictions[-1][0]#get outs
         else :
             return self.predictions[-1]
     
     def back(self,y):#back propagate error
-        
+     #   print(len(Y),"iiiiii")
         self.derivateR=[(derivate(self.predictions[-1][s],y[s]) * self.lr)for s in range(len(y))]
         self.momentum[-1]=[[self.derivateR[i]* self.m] for i in range(len(self.derivateR))]
         self.bias[-1] = self.derivateR
@@ -174,16 +234,16 @@ class neural_network():
                            self.momentum[q][x] = self.derivatesW[q+1][x]* self.m
                            self.bias[q][x] = self.derivatesW[q+1][x]
                            self.derivatesW[q][x] = self.weights[q][x][0] - (self.weights[q][x][0]*self.predictions[q][x]*self.derivatesW[q+1][x])
-
+                    
                      else:
                     #     print("<",len(self.derivatesW[q+1]) / len(self.derivatesW[q]))
                          for t in range(len(self.derivatesW[q+1])):
-                                    self.momentum[q][x] = self.derivatesW[q+1][x]* self.m
+                                    self.momentum[q][x] = self.derivatesW[q+1][t]* self.m
                                     self.bias[q][x] = self.derivatesW[q+1][t]
                                     self.derivatesW[q][x] = self.weights[q][x][0] - (self.weights[q][x][0]*self.predictions[q][x]*self.derivatesW[q+1][t])+self.momentum[q][t]
                  if math.isinf(self.derivatesW[q][x]) == True or math.isnan(self.derivatesW[q][x]) ==True  or  math.isnan(self.weights[q][x][0]) == True or math.isinf(self.derivatesW[q][x]) == True or math.isnan(self.derivatesW[q][x]) == True:
 
-                    self.derivatesW[q][x] = 0.0001
+                    self.derivatesW[q][x] = 0.00001
                     self.weights[q][x] =initW(1)
                  else:
                      self.weights[q][x] = [self.derivatesW[q][x]]
